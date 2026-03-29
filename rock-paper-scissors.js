6a1efcaf4dcd8ac717a6b8e3af30b6efcd85a2ef
KreoSteel <tatarchukluca@gmail.com>
2026-03-29 20:04:52 +0200

    Add HTML structure, and finished .js file

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,64 +1,129 @@
-// Initialize scores
 let playerScore = 0;
 let computerScore = 0;
 
-// Randomize and return "computer" choise
+let roundsToPlay = 5;
+
+// Select elements
+const playBtn = document.querySelector("#play");
+const playDiv = document.querySelector("#play-container");
+
+// Create elements
+const chooseText = document.createElement("h3");
+const playerBtnsDiv = document.createElement("div");
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+
+const scoresDiv = document.createElement("div");
+const yourScore = document.createElement("p");
+
+const topScoresDiv = document.createElement("div");
+const compScore = document.createElement("p");
+const gameScore = document.createElement("p");
+
+rockBtn.classList.add("play-buttons");
+paperBtn.classList.add("play-buttons");
+scissorsBtn.classList.add("play-buttons");
+
+rockBtn.innerHTML = '<img src="images/rock.png" alt=""> Rock';
+paperBtn.innerHTML = '<img src="images/paper.png" alt=""> Paper';
+scissorsBtn.innerHTML = '<img src="images/scissors.png" alt=""> Scissors';
+rockBtn.dataset.choice = "rock";
+paperBtn.dataset.choice = "paper";
+scissorsBtn.dataset.choice = "scissors";
+
+yourScore.textContent = "Your score: 0";
+compScore.textContent = "Computer score: 0";
+gameScore.textContent = "Game score: 0 : 0";
+
+playerBtnsDiv.classList.add("player-buttons");
+scoresDiv.classList.add("scores");
+
 function getComputerChoice() {
-   const rockPaperScissors = Math.floor(Math.random() * 3);
-   switch (rockPaperScissors) {
-      case 0:
-         return "rock";
-      case 1:
-         return "paper";
-      case 2:
-         return "scissors";
-   }
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
-// Returns player's choise
-function getPlayerChoice() {
-   const choise = prompt("Your turn! (Rock, Paper or Scissors)");
-   return choise.toLowerCase();
+function addScore(player, computer) {
+  if (player === computer) {
+    chooseText.textContent = `${player} : ${computer}`;
+  } else if (player === "rock" && computer === "scissors") {
+    playerScore++;
+    chooseText.textContent = `${player} : ${computer}`;
+  } else if (player === "paper" && computer === "rock") {
+    playerScore++;
+    chooseText.textContent = `${player} : ${computer}`;
+  } else if (player === "scissors" && computer === "paper") {
+    playerScore++;
+    chooseText.textContent = `${player} : ${computer}`;
+  } else {
+    computerScore++;
+    chooseText.textContent = `${player} : ${computer}`;
+  }
 }
 
-// Decide who wins or draw
-function playRound(player, computer) {
-   if (player === computer) {
-      console.log("Draw!");
-   } else if (player === "rock" && computer === "scissors") {
-      playerScore++;
-   } else if (player === "paper" && computer === "rock") {
-      playerScore++;
-   } else if (player === "scissors" && computer === "paper") {
-      playerScore++;
-   } else computerScore++;
-   console.log(`P: ${playerScore}, C: ${computerScore}`);
+function buildGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundsToPlay = 5;
+  updateUIScore();
+
+  chooseText.textContent = "Choose!";
+  playDiv.innerHTML = "";
+
+  playerBtnsDiv.append(rockBtn, paperBtn, scissorsBtn);
+  topScoresDiv.append(yourScore, compScore);
+  scoresDiv.append(topScoresDiv, gameScore);
+  playDiv.append(chooseText, playerBtnsDiv, scoresDiv);
 }
 
-// Repeat playRound 5 times and show final results
-function playGame() {
-   for (let i = 0; i < 5; i++) {
-      const playerChoise = getPlayerChoice();
-      const computerChoise = getComputerChoice();
-      console.log(`Player ${playerChoise}, Computer ${computerChoise}`);
-      
-      playRound(playerChoise, computerChoise);
-   }
+function finishGame() {
+  playDiv.innerHTML = "";
+  const par = document.createElement("p");
+  const restartBtn = document.createElement("button");
+  const gameScoreMsg = `Game score ${playerScore} : ${computerScore}`;
 
-   if (playerScore === computerScore) {
-      console.log(
-         `Its a draw! Final score: P: ${playerScore}, C: ${computerScore}`,
-      );
-   } else if (playerScore > computerScore) {
-      console.log(
-         `Player wins! Final score: P: ${playerScore}, C: ${computerScore}`,
-      );
-   } else {
-      console.log(
-         `Computer wins! Final score: P: ${playerScore}, C: ${computerScore}`,
-      );
-   }
+  restartBtn.textContent = "Restart";
+  restartBtn.addEventListener("click", () => {
+    buildGame();
+  });
+
+  if (playerScore === computerScore) {
+    par.innerHTML = `Draw! <br> ${gameScoreMsg}`;
+  } else if (playerScore > computerScore) {
+    par.innerHTML = `Player wins! <br> ${gameScoreMsg}`;
+  } else if (playerScore < computerScore) {
+    par.innerHTML = `Computer wins! <br> ${gameScoreMsg}`;
+  }
+
+  playDiv.classList.add("game-over");
+  playDiv.append(par, restartBtn);
 }
 
-// Calling the playGame();
-playGame();
+function updateUIScore() {
+  yourScore.textContent = `Your score: ${playerScore}`;
+  compScore.textContent = `Computer score: ${computerScore}`;
+  gameScore.textContent = `Game score: ${playerScore} : ${computerScore}`;
+}
+
+function handlePlayerChoice(button) {
+  const computerChoice = getComputerChoice();
+  const playerChoice = button.dataset.choice;
+
+  addScore(playerChoice, computerChoice);
+  updateUIScore();
+  --roundsToPlay;
+
+  if (roundsToPlay <= 0) {
+    finishGame();
+  }
+}
+
+playBtn.addEventListener('click', () => {
+  playDiv.classList.remove('game-over');
+  buildGame();
+}, { once: true });
+
+rockBtn.addEventListener("click", () => handlePlayerChoice(rockBtn));
+paperBtn.addEventListener("click", () => handlePlayerChoice(paperBtn));
+scissorsBtn.addEventListener("click", () => handlePlayerChoice(scissorsBtn));
